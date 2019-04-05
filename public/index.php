6c0d4cb42bcdfb38fbc0cdcd8ce673dcad0b35c0
Vehemence26 <45357640+Vehemence26@users.noreply.github.com>
2019-04-05 12:48:22 +0800

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,14 +1,96 @@
-<!doctype html>
-
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>PAYROLL WEBSITE</title>
-    <meta charset="utf-8">
-  </head>
+<head>
+	<title>Sands Fridge Lines</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" type="text/css" href="stylesheets/main2.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container-fluid">	 	<!--  wrapper container START-->
 
-  <body>
+		<div class="header-top row"> 	<!-- header-top row START -->
+			<div class="logo col-lg-4 col-md-12"> <!-- nav container START -->
+			    <h1>Sands</h1>  
+			    <h2>Fridge lines</h2>  
+			</div>
+			<div class=" col-lg-8 col-md-12 container-fluid">
+				<nav class="top-nav row">
+					<ul class="col-12">
+						<li> <a href="">HISTORY</a> </li>
+						<li> <a href="">LOCATIONS</a> </li>
+						<li> <a href="">CONTACT</a> </li>
+						<li> <a href="">HOME</a> </li>
+						<li id="login"> <a href="schedules/index.php">MEMBER LOGIN</a> </li>
+					</ul>
+				</nav>
+				<nav class="main-nav row ">
+					<ul class="col-12">
+						<li> <a href="">TRANSPORT SERVICES</a> </li>
+						<li> <a href="">WAREHOUSES</a> </li>
+						<li> <a href="">SCHEDULES</a> </li>
+						<li> <a href="">OUR FLEET</a> </li>
+								
+					</ul>
+				</nav>
+			</div>	<!-- nav container END -->
+		</div> <!-- header-top row END -->
+	</div> <!--  wrapper container END-->
 
-    <h1>UNDER CONSTRUCTION || We are coming soon</h1>
+		<div class="header-bottom container-fluid "> <!-- header bottom container START -->
+			<div class="row"> 
+				<div class="contact-info align-items-center col-lg-4 col-md-12">
+					<p> 
+						<strong>Perth Terminal</strong> <br> 
 
-  </body>
+						493 Dundas Rd, Forrestfield, Western Australia 6058 <br>
+
+						<strong>GET A QUOTE TODAY! <br> 
+						Tel: 08 9454 8770</strong> <br> 
+
+						Fax: 08 9454 4574
+					</p>
+				</div>
+			<!-- <img class="banner-img col-lg-8 col-md-12" src="road-train.jpg" alt="banner image of truck driver" height='200'> -->	
+			<div class="banner-img col-lg-8 col-md-12"><img src="images/road-train.jpg" id="banner" alt="banner image of truck" height="200px" width="100%"></div>
+
+	</div> <!-- header bottom container END -->
+
+	<div class="main container-fluid"> <!-- main container START -->
+		<div class="row"> <!-- main row START -->
+
+			<div class="intro col-lg-8 col-md-12">
+				<h3>EXCELLENCE IN</h3>
+				<h4>REFRIGERATED WARHOUSING AND DISTRIBUTION</h4>
+				<hr >
+				<p>Sands Fridge Lines has achieved its position at the forefront of WA's refrigerated distribution industry by strategic planning based on 25 years experience in our specialist field.</p>
+				<hr>
+				<h4>MISSION STATEMENT</h4>
+				<p>Our goal is to be dedicated to providing the customer unparalleled levels of quality service and value, and to achieve a standard of excellence with developing the culture and heritage of our business in the services of refrigerated road distribution.</p>
+			</div>
+
+			<div class="locations col-lg-4 col-md-12" >
+					<h4>OUR LOCATIONS</h4>
+					<img src="images/wa.gif" alt="map of western Australia" width="300px" height="400px">
+			</div>
+		
+		</div>  <!-- main row END -->
+	</div> <!-- main container END -->
+
+	<div class="footer container-fluid"> <!-- footer container START -->
+		<div class="row"> <!-- footer row START -->
+
+				<footer class="col-12">
+					Copyright &copy <?php echo date("Y"); ?><br>
+					Designed by Brendan Gilet.
+				</footer>
+			
+		</div> <!-- footer row END -->
+	</div> <!-- footer container END -->
+
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
